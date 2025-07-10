72c1d2e396e9bb4e51a87b342a2924dad1106719
Nico Conathan-leach <nico.cleach@gmail.com>
2025-07-10 16:43:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
-import { getFirestore, type Firestore } from "firestore";
+import { getFirestore, type Firestore } from "firebase/firestore";
 
 // **IMPORTANT**: This configuration now points *exclusively* to your 'rankle-7db08' project.
 // This is the single source of truth for your client-side Firebase connection.
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB-X_de5SUn73RM3FQPSMLhnUc311hK5Ic",
   authDomain: "rankle-7db08.firebaseapp.com",
   projectId: "rankle-7db08",
-  storageBucket: "rankle-7db08.firebasestorage.app",
+  storageBucket: "rankle-7db08.appspot.com",
   messagingSenderId: "169560119600",
   appId: "1:169560119600:web:b000b93427c209ec3a50f5",
 };
